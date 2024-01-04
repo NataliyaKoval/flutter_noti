9d44bcf12709db63a671da6abca6964543f13164
NataliiaKoval <hakunata@gmail.com>
2024-01-04 16:48:10 +0100

    Create TabBar

---
+++ lib/consts/notifications_screen_strings.dart
@@ -0,0 +1,5 @@
+class NotificationsScreenStrings {
+  String get title => 'Notifications';
+  String get firstTab => 'One-time';
+  String get secondTab => 'Recurring';
+}
--- lib/presentation/notifications_screen/notifications_page.dart
+++ lib/presentation/notifications_screen/notifications_page.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:noti/consts/app_colors.dart';
+import 'package:noti/consts/strings.dart';
+import 'package:noti/presentation/notifications_screen/widgets/custom_tab.dart';
 
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 2,
+      child: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 78,
+            backgroundColor: AppColors.eerieBlack,
+            centerTitle: true,
+            title: Text(
+              Strings.notificationsScreenStrings.title,
+              style: const TextStyle(
+                color: AppColors.white,
+                fontSize: 16,
+              ),
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size(400, 62),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  height: 48,
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: TabBar(
+                    tabs: [
+                      CustomTab(
+                        text: Strings.notificationsScreenStrings.firstTab,
+                        iconData: Icons.timer_outlined,
+                      ),
+                      CustomTab(
+                        text: Strings.notificationsScreenStrings.secondTab,
+                        iconData: Icons.history,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          body: TabBarView(
+            //controller: _tabController,
+            children: [
+              Container(
+                color: Colors.yellow,
+              ),
+              Container(
+                color: Colors.green,
+              ),
+            ],
+          ),
+        );
+      }),
+    );
   }
 }
